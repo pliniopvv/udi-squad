436876ab1e700db2bcfb14c13952a4dfe3238443
Plínio Pvv <gangss2@hotmail.com>
2023-11-12 19:56:35 -0300

    publicando

--- package_publish.bat
+++ package_publish.bat
@@ -1,5 +1,12 @@
+@echo off
+cls
+
 start /wait cmd /C build.bat
-start /wait cmd /C ncc build backend/dist/main.js -o publish/backend
+start /wait cmd /C ncc build backend/build/main.js -o publish/backend
 RMDIR /S /Q publish/frontend
 RMDIR /S /Q publish/backend
 XCOPY "backend/public_html" "publish/public_html/*" /E /Y /J
+
+cd backend
+copy .env ..\publish\backend
+cd ..
