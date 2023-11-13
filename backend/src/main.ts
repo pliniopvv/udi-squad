c76cc047d426b598e0cf7ceb81ef09c0e881c061
Plínio Pvv <gangss2@hotmail.com>
2023-11-12 21:10:57 -0300

    fix login

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
+  app.enableCors();
   await app.listen(parseInt(env.PORT));
 }
 bootstrap();
