07f27c09ed30f3d4e67c92325ece383cf41b36ac
Plínio Pvv <gangss2@hotmail.com>
2023-11-17 11:13:58 -0300

    enter to <br />

--- frontend/src/app/logged/components/feedcard/feedcard.component.ts
+++ frontend/src/app/logged/components/feedcard/feedcard.component.ts
@@ -4,20 +4,25 @@ import { Feedcard } from 'src/app/model/feedcard';
 @Component({
   selector: 'app-feedcard',
   templateUrl: './feedcard.component.html',
-  styleUrls: ['./feedcard.component.scss']
+  styleUrls: ['./feedcard.component.scss'],
 })
 export class FeedcardComponent implements OnInit {
-
   @Input('card')
   card: Feedcard;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   setCard(card: Feedcard) {
     this.card = card;
   }
 
+  fenter(body: String) {
+    if (body.indexOf('<script>') > 0 || body.indexOf('</script>') > 0)
+      return 'Favor evitar utilizar as tags html <script></script>.';
+
+      //@ts-ignore
+    return body.replaceAll('\n', '<br />');
+  }
 }
